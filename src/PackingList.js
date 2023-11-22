da9dfd89e5f1b9bf5508c649d93a82e0f7769e5e
Hamid962 <hamidrafiq962@gmail.com>
2023-11-22 19:44:13 +0500

    removing extra code

--- src/PackingList.js
+++ src/PackingList.js
@@ -7,26 +7,10 @@ export default function PackingList({
   onToggleItem,
   onClearList,
 }) {
-  const [sortBy, setSortBy] = useState("input");
-
-  let sortedItems;
-
-  if (sortBy === "input") sortedItems = items;
-
-  if (sortBy === "description")
-    sortedItems = items
-      .slice()
-      .sort((a, b) => a.description.localeCompare(b.description));
-
-  if (sortBy === "packed")
-    sortedItems = items
-      .slice()
-      .sort((a, b) => Number(a.packed) - Number(b.packed));
-
   return (
     <div className="list">
       <ul>
-        {sortedItems.map((item) => (
+        {items.map((item) => (
           <Item
             item={item}
             onDeleteItem={onDeleteItem}
